d96952f42a55b7352a7204219413324fd49ff42f
im <poson_hemp@yahoo.co.jp>
2023-06-13 09:33:58 +0900

    prepare/ mock server (GET)

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	printMessage()
+	getHelloHandler()
 }
 
-func printMessage() {
-	fmt.Println("Hello world.")
+func getHelloHandler() {
+	e := echo.New()
+
+	e.GET("/", helloHandler)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func helloHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello world.")
 }
